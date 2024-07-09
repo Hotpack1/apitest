f5f63d70ec590d409e2f28ecac6ca36459af4e9c
keunpyo jeon <john.coner72@gmail.com>
2024-07-09 21:15:19 +0900

    second-commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_KEY = ``
 const getLatestNews = async () => {
     const url = new URL(
-        ``
+        `https://kp-news-api.netlify.app/`
     );
     const response = await fetch(url);
     const data = await response.json();
