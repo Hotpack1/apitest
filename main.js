3e1f6d9892a1bb3f1d74e6cd3a49f85a87724e50
keunpyo jeon <john.coner72@gmail.com>
2024-07-09 21:25:13 +0900

    second-fixed

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 //const API_KEY = `52a44242974f42108a4b865ae7206ff6`
 let news=[]
 const getLatestNews = async () => {
-    const url = new URL(
-        `https://kp-news-api.netlify.app/top-headlines`
-    );
+   // const url = new URL(`https://kp-news-api.netlify.app/top-headlines`);
+    let url = `https://kp-news-api.netlify.app/top-headlines`
     const response = await fetch(url);
     const data = await response.json();
     news = data.articles
