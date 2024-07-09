f5b962009fa578094ed319ae5a3ecdd34fca1938
keunpyo jeon <john.coner72@gmail.com>
2024-07-09 21:21:58 +0900

    fixed-second-commit

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-const API_KEY = ``
+//const API_KEY = `52a44242974f42108a4b865ae7206ff6`
+let news=[]
 const getLatestNews = async () => {
     const url = new URL(
-        `https://kp-news-api.netlify.app/`
+        `https://kp-news-api.netlify.app/top-headlines`
     );
     const response = await fetch(url);
     const data = await response.json();
-    console.log("rrrrr",response);
+    news = data.articles
+    console.log("rrrrr",news);
 }
 
 getLatestNews();
